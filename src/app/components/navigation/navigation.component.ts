02aa7355d8574674f4ff9aa57869b8fb632e9946
Sia-WRWD <chinojen7@gmail.com>
2023-05-28 10:37:19 +0800

    Added Fix for Nav Sidebar Not Closable on 280 width devices

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { UntypedFormControl } from '@angular/forms';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navigation',
@@ -15,25 +16,26 @@ export class NavigationComponent {
   @HostBinding('class') className = '';
   toggleControl = new UntypedFormControl("");
   themes = [
-    {name: "Light Theme", value: ""},
-    {name: "Dark Theme", value: "Dark Theme"},
-    {name: "Bocchi Theme", value: "Bocchi Theme"}
+    { name: "Light Theme", value: "" },
+    { name: "Dark Theme", value: "Dark Theme" },
+    { name: "Bocchi Theme", value: "Bocchi Theme" }
   ];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  @ViewChild(MatSidenav, { static: false }) sidenav!: MatSidenav;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private overlayContainer: OverlayContainer,
-    ) {}
+  ) { }
 
   ngOnInit(): void {
     this.changeTheme();
   }
-  
+
   changeTheme() {
     this.toggleControl.valueChanges.subscribe(val => {
       console.log(val);
@@ -60,7 +62,7 @@ export class NavigationComponent {
         classes.add(bocchiThemeClass);
         classes.remove(darkModeClass);
       }
-      
+
       this.changeBackgroundImage(val);
       this.changeOverlayVisibility(val);
     })
@@ -100,53 +102,66 @@ export class NavigationComponent {
     }
   }
 
+  closeSidebar() {
+    setTimeout(() => {
+      this.sidenav.close();
+    }, 1000);
+  }
+
   directIntro() {
     let x = document.querySelector("#intro");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directAbout() {
     let x = document.querySelector("#about");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directSkills() {
     let x = document.querySelector("#skills");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directEdu() {
     let x = document.querySelector("#edu");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directProj() {
     let x = document.querySelector("#proj");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directExp() {
     let x = document.querySelector("#exp");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
   directContact() {
     let x = document.querySelector("#contact");
     if (x) {
-      x.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
+    this.closeSidebar();
   }
 
 }
