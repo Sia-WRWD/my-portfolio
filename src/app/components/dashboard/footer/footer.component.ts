a38c0ef6cb152315d21aa46c8f58365b5c601789
Sia-WRWD <chinojen7@gmail.com>
2023-05-30 23:03:14 +0800

    Completed Footer, Marking the End for now.

--- src/app/components/dashboard/footer/footer.component.ts
+++ src/app/components/dashboard/footer/footer.component.ts
@@ -12,4 +12,53 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  directIntro() {
+    let x = document.querySelector("#intro");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directAbout() {
+    let x = document.querySelector("#about");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directSkills() {
+    let x = document.querySelector("#skills");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directEdu() {
+    let x = document.querySelector("#edu");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directProj() {
+    let x = document.querySelector("#proj");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directExp() {
+    let x = document.querySelector("#exp");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
+  directContact() {
+    let x = document.querySelector("#contact");
+    if (x) {
+      x.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
+  }
+
 }
